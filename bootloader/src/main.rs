ffd771081bc693002f7c67f4a0d12622ae66e611
Noah <noah@Virtuelle-Maschine-von-Noah.local>
2026-03-23 18:40:30 +0100

    feat(bootloader): hello uefi output

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use uefi::prelude::*;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn efi_main(_image_handle: usize, _system_table: usize) -> usize {
+entry!(efi_main);
+
+fn efi_main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    let _ = system_table.stdout().reset(false);
+    let _ = system_table
+        .stdout()
+        .write_str("Project N.N. UEFI bootloader\r\n");
+    let _ = system_table.stdout().write_str("Hello UEFI\r\n");
+
     // TODO: initialize UEFI services, load kernel, exit boot services, jump to kernel entry.
-    0
+    Status::SUCCESS
 }
